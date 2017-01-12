176b83def76a2a9592d0c56d826004df1024d9ff
Vermillion <tjc0609@gmail.com>
2017-01-11 20:04:24 -0800

    Starting the utility functionality process

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,13 @@ module.exports = (express) => {
   });
 
   // API Routes
+  // Version 1 - Lacked version control in the path
   router.use('/api/', require('./api/user')(express));
   router.use('/api/', require('./api/url')(express));
+  // Version 2 - Version Control to path
+  router.use('/api/v2/', require('./api/v2/user')(express));
+  router.use('/api/v2/', require('./api/v2/url')(express));
+
   // Route for getting the original url redirect.
   router.use('/go/', require('./go')(express));
 
--- src/test/_apps.js
+++ src/test/_apps.js
@@ -1,9 +1,9 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 
-describe('App Routes', () => {
+describe('URL Routes', () => {
   var server;
-  var app;
+  var url;
 
   beforeEach(() => {
     server = require('../src/server.js');
@@ -14,34 +14,37 @@ describe('App Routes', () => {
   });
 
   // Test for Multiple Apps
-  it('GET /api/v1/apps returns multiple apps', (done) => {
+  it('GET /api/v1/urls returns multiple urls', (done) => {
     request(server)
-      .get('/api/v1/apps')
-      .set('Accept', 'application/json')
+      .get('/api/v1/urls')
+      .set('Accept', 'urllication/json')
       .expect('Content-Type', /json/)
       .expect((res) => {
-        const apps = res.body;
+        const urls = res.body;
 
-        // Save one single app from the list to test on in later tests
-        this.app = apps[0]
+        // Save one single url from the list to test on in later tests
+        this.url = urls[0]
 
-        expect(apps.length).to.be.above(0)
+        expect(urls.length).to.be.above(0)
       })
       .end(done)
   });
 
-  // Test for a single app
-  it('GET /api/v1/apps/:id returns an app obj with id, title, description, and releaseDate properties', (done) => {
+  // Test for a single url
+  it('GET /api/v1/urls/:id returns an url obj with id, title, description, and releaseDate properties', (done) => {
     request(server)
-      .get('/api/v1/apps/' + this.app.id)
-      .set('Accept', 'application/json')
+      .get('/api/v1/urls/' + this.url.id)
+      .set('Accept', 'urllication/json')
       .expect('Content-Type', /json/)
       .expect((res) => {
-        const app = res.body;
-        expect(app).to.have.property('id')
-        expect(app).to.have.property('title')
-        expect(app).to.have.property('description')
-        expect(app).to.have.property('releaseDate')
+        const url = res.body;
+        expect(url).to.have.property('id')
+        expect(url).to.have.property('originalUrl')
+        expect(url).to.have.property('shortUrl')
+        expect(url).to.have.property('urlName')
+        expect(url).to.have.property('createdAt')
+        expect(url).to.have.property('updatedAt')
+        expect(url).to.have.property('userID')
       })
       .end(done)
   });
